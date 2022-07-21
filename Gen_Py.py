9f3b067eec4f6bc9db3142a449a10dbb0fab1975
Jesse Ward-Bond <JesseWB.C@gmail.com>
2022-07-21 18:20:42 -0400

    Now correctly generates points

--- Gen_Py.py
+++ Gen_Py.py
@@ -4,49 +4,58 @@ import numpy as np
 # Calculate the total area formed
 
 def area(x1,y1,x2,y2,x3,y3):
-    return abs((x1 * (y1-y3) + x2 * (y3-y1) + x3 * (y1-y2)) / 2.0)
+    return abs((x1 * (y2-y3) + x2 * (y3-y1) + x3 * (y1-y2)) / 2.0)
 
-def checkArea(x1,y1,x2,y2,x3,y3,x,y):
+def isInside(x1,y1,x2,y2,x3,y3,x,y):
     A = area (x1, y1, x2, y2, x3, y3) # Total Area
     A1 = area (x, y, x2, y2, x3, y3)
     A2 = area (x1, y1, x, y, x3, y3)
     A3 = area (x1, y1, x2, y2, x, y)
+    tmp = A1 + A2 + A3
+    tmp2 = tmp==A
     if (A == A1 + A2 + A3):
         return True
     else:
         return False
 
-def isInside(x1,y1,x2,y2,x3,y3,x,y):
-    if (checkArea(0, 0, 20, 0, 10, 30, 10, 15)):
-        # print('Inside')
-        return True
-    else:
-        # print('Not Inside')
-        return False
+    
+def get_acute_angles(x1, y1, x2, y2, x3, y3):
+    H = ((x3-x1)**2 + (y2-y1)**2)**(1/2)
+    theta_123 = np.arcsin((x3-x1)/H)
+    theta_132 = np.arcsin((y2-y1)/H)
+    
+    return theta_123, theta_132
 
-x_max = 50  # Point (x_max, 0.0)
-y_max = 50  # Point (0.0, y_max)
-a = 0.0    # x coordinate
-c = 0
-store = []
-
-for a in np.arange(0, 2.5, x_max):
-    b = 0.0  # y coordinate
-    for b in np.arange(0,2.5, y_max):
-        if isInside(0.0,0.0, 0.0,y_max, x_max,0.0, a,b):
-            print("Point %d, %d is inside", a, b)
-            store[c] = [a,b]
-            c = c+1
-        else:
-            break
-
-d = 0
-for d in range(0,c):
-    print (store[d])
-    print('\n')
-    d = d+1
+def get_deltas(step, x1, y1, x2, y2, x3, y3):
+    theta_123, theta_132 = get_acute_angles(x1, y1, x2, y2, x3, y3)
+    
+    delta_x = step / np.tan(theta_132/2)
+    delta_y = step / np.tan(theta_123/2)
+    
+    return delta_x, delta_y
 
 
+def fn():
+
+    x_max = 50  # Point (x_max, 0.0)
+    y_max = 50  # Point (0.0, y_max)a
+    step = 2.5
+    store = []
+
+    delta_x, delta_y = get_deltas(step, 0, 0, 0, y_max, x_max, 0)
+
+    for a in np.arange(0+step, x_max+step-delta_x, step):
+        for b in np.arange(0+step, y_max+step-delta_y, step):
+            if isInside(step, step, step,y_max-delta_y, x_max-delta_x,step, a,b):
+                store.append([a,b])
+
+
+    store.append([step, y_max-delta_y])
+    store.append([x_max-delta_x, step])
+    return store
 
 
 
+
+if __name__ == '__main__':
+    print(fn())
