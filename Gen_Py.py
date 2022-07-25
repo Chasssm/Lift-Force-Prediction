5c59c9f4b3829de025a465955c2912f0e8466351
Jesse Ward-Bond <JesseWB.C@gmail.com>
2022-07-25 15:32:41 -0400

    Added comment

--- Gen_Py.py
+++ Gen_Py.py
@@ -2,6 +2,8 @@ import numpy as np
 
 # Function that calculates the AREA of triangle given 3 coordinates
 # Using Cross Product
+#Hello Yifei
+
 def area(x1,y1,x2,y2,x3,y3):
     return abs((x1 * (y2-y3) + x2 * (y3-y1) + x3 * (y1-y2)) / 2.0)
 
