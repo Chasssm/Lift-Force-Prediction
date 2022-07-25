2c852bf148b99c49155549b957b108481ffda62a
Jesse Ward-Bond <JesseWB.C@gmail.com>
2022-07-25 15:28:52 -0400

    Added a comment

--- Gen_Py.py
+++ Gen_Py.py
@@ -3,6 +3,8 @@ import numpy as np
 # A function that checks if a point lies inside a trangle
 # Calculate the total area formed
 
+#Hello Yifei
+
 def area(x1,y1,x2,y2,x3,y3):
     return abs((x1 * (y2-y3) + x2 * (y3-y1) + x3 * (y1-y2)) / 2.0)
 
