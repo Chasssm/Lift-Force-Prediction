bdc1aff9a1254357f7a87dbd8007c17e261573c4
Chasssm <yifeiwumary@outlook.com>
2022-08-17 15:29:32 -0400

    Completed Debugging

--- test2.py
+++ test2.py
@@ -1,7 +1,25 @@
-for i in range(4):
-    print(i)
-    i = i + 1
+import numpy as np
+from matplotlib import pyplot as plt
 
-new_coor = [[1, 2], [3, 4], [5, 6]]
+x_max = 200
+y_max = 100
+s = [2.5, 5, 8, 10]
+N = [50, 40, 40, 20]
 
-print(new_coor[0][0])
+A = [0,0]
+B = [0,y_max]
+C = [x_max, 0]
+store = []
+
+theta_b = np.arctan(x_max/y_max)
+alpha = (np.pi/2-theta_b)/2  # 1/2 theta_c
+
+def get_new_triangle(A, B, C, s):
+    delta_b = s/np.tan(theta_b)+s/np.sin(theta_b)
+    delta_c = s/np.tan(alpha)
+    A1 = [(A[0]+s),(A[1]+s)]
+    B1 = [(B[0]+s),(B[1]-delta_b)]
+    C1 = [(C[0]-delta_c),(C[1]+s)]
+    return [A1, B1, C1]
+
+print(get_new_triangle(A, B, C, s[0]))
